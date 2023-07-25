700b818ee679145a9b5c45e5687ea3753b5c5c2a
Petr Babkin <babkin.pk@phystech.edu>
2023-07-26 00:23:53 +0300

    final

--- supernet.py
+++ supernet.py
@@ -6,8 +6,8 @@ import torch.nn as nn
 
 class Sample(nn.Module):
     '''
-    Шаблон класса подмодели суперсети.
-    Подмодель имеет вид: in -> layer1 -> [block1] -> layer2 -> [block2] -> global_avg_pool -> linear -> out.
+    Шаблон класса экземпляра суперсети.
+    Экземпляра имеет вид: in -> layer1 -> [block1] -> layer2 -> [block2] -> global_avg_pool -> linear -> out.
     [block] может состоять из 1, 2 или 3 сверток.
     '''
     def __init__(self): 
@@ -78,34 +78,36 @@ class SuperNet():
         torch.nn.init.uniform_(self.fc.weight, -1, 1)
         torch.nn.init.uniform_(self.fc.bias, -1, 1)
 
-    def sample(self, submod):
+    def _sample(self, submod):
         '''
         Возвращает одну из подмоделей суперсети.
 
-        :param submod = (x, y): номер подмодели. x, y лежат от 1 до 3. x отвечает за первый изменяемый блок, y за второй.
+        :param submod = (x, y): номер подмодели. x, y лежат от 0 до 2. x отвечает за первый изменяемый блок, y за второй.
         '''
         x, y = submod
-        assert 1 <= x <= 3 and 1 <= y <= 3, "Некорректное задание модели: 1 <= x, y <= 3"
+        assert 0 <= x <= 2 and 0 <= y <= 2, "Некорректное задание модели: 0 <= x, y <= 2"
 
         sam = Sample()
         sam.layer1 = self.layer1
-        sam.block1 = self.block1[0 : x]
+        sam.block1 = self.block1[0 : x + 1]
         sam.layer2 = self.layer2
-        sam.block2 = self.block2[0 : y]
+        sam.block2 = self.block2[0 : y + 1]
         sam.global_avg_pool = self.global_avg_pool
         sam.fc = self.fc
 
         return sam
 
-    def train_sample(self, submod, train_loader, num_epochs = 5, learning_rate = 0.001):
+    def train_sample(self, submod, train_loader, num_epochs = 5, learning_rate = 0.001, process=True):
         '''
         Обучение конкретного экземпляра.
 
-        :param submod = (x, y): номер подмодели. x, y лежат от 1 до 3. x отвечает за первый изменяемый блок, y за второй.
+        :param submod = (x, y): номер подмодели. x, y лежат от 0 до 2. x отвечает за первый изменяемый блок, y за второй.
         :param train_loader: загрузчик обучающих данных.
+        :param process: переменная, говорящая выводить ли информацию о процессе обучения.
         '''
         criterion = nn.CrossEntropyLoss()
-        model = self.sample(submod)
+        model = self._sample(submod)
+
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 
         total_step = len(train_loader)
@@ -125,10 +127,12 @@ class SuperNet():
                 correct = (predicted == labels).sum().item()
                 acc_list.append(correct / total)
 
-                if (i + 1) % 200 == 0:
+                if (i + 1) % 300 == 0 and process:
                     print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'.format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), (correct / total) * 100))
 
         return loss_list, acc_list
+    
+    
     
     def train(self, train_loader, num_epochs = 5, learning_rate = 0.001):
         '''
@@ -142,13 +146,13 @@ class SuperNet():
         acc_lists = [[] for _ in range(9)]
         for epoch in range(num_epochs):
             for i, (images, labels) in enumerate(train_loader):
-                x, y = torch.randint(3, (2,)) + 1
-                model = self.sample((x, y))
+                x, y = torch.randint(3, (2,))
+                model = self._sample((x, y))
                 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
                 
                 outputs = model(images)
                 loss = criterion(outputs, labels)
-                loss_lists[(x - 1) * 3 + (y - 1)].append(loss.item())
+                loss_lists[x * 3 + y].append(loss.item())
 
                 optimizer.zero_grad()
                 loss.backward()
@@ -157,29 +161,57 @@ class SuperNet():
                 total = labels.size(0)
                 _, predicted = torch.max(outputs.data, 1)
                 correct = (predicted == labels).sum().item()
-                acc_lists[(x - 1) * 3 + (y - 1)].append(1.0 * correct / total)
+                acc_lists[x * 3 + y].append(1.0 * correct / total)
 
-                if (i + 1) % 200 == 0:
+                if (i + 1) % 300 == 0:
                     print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'.format(epoch + 1, num_epochs, i + 1, total_step, loss.item(), (correct / total) * 100))
 
         return loss_lists, acc_lists
     
     def validate_sample(self, submod, test_loader):
         '''
-        Обучение конкретного экземпляра.
+        Валидация конкретного экземпляра.
 
-        :param submod = (x, y): номер подмодели. x, y лежат от 1 до 3. x отвечает за первый изменяемый блок, y за второй.
+        :param submod = (x, y): номер подмодели. x, y лежат от 0 до 2. x отвечает за первый изменяемый блок, y за второй.
         :param train_loader: загрузчик обучающих данных.
         '''
-        model = self.sample(submod)
+        model = self._sample(submod)
         with torch.no_grad():
             correct = 0
             total = 0
             for images, labels in test_loader:
+
                 outputs = model(images)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
-            print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
+            print('Test Accuracy of the model ({}, {}) on the 10000 test images: {} %'.format(submod[0], submod[1], (correct / total) * 100))
 
+    def validate_ensemble(self, test_loader):
+        '''
+        Валидация ансамбля моделей из всех сетей вместе.
+
+        :param submod = (x, y): номер подмодели. x, y лежат от 0 до 2. x отвечает за первый изменяемый блок, y за второй.
+        :param train_loader: загрузчик обучающих данных.
+        '''
+        models = []
+        for x in range(3):
+            for y in range(3):
+                models.append(self._sample((x, y)))
+                models[x * 3 + y].eval()
+
+        with torch.no_grad():
+            correct = 0
+            total = 0
+            for images, labels in test_loader:
+
+                outputs = torch.zeros(100, 10)
+                for model in models:
+                    outputs += model(images)
+                outputs = outputs / 10
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+            print('Test Accuracy of the ensemle on the 10000 test images: {} %'.format((correct / total) * 100))
